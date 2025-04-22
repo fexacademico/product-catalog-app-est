a3c6341fb4067de3451273dc2d0b1fa594be6f5b
Felix Lee <fexacademico@gmail.com>
2025-04-22 06:14:15 -0600

    feat: add blank product application for the students.

---
+++ script.js
@@ -0,0 +1,12 @@
+// Utilice este diseño para las tarjetas del catalogo
+
+/* <div class="card">
+  <div>
+    <h3>${item.name}</h3>
+    <img src="${item.imageUrl}" alt="${item.name}">
+    <p>${item.description}</p>
+  </div>
+  <div class="card-footer">
+    <button onclick="deleteProduct(${item.id})">Eliminar</button>
+  </div>
+</div> */
